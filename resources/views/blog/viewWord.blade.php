d6781a76e16680e83cc0d56e2d7420fafdf8f118
Javohir <jasdobilov@yandex.ru>
2023-10-15 10:25:54 +0500

    added file name

--- resources/views/blog/viewWord.blade.php
+++ resources/views/blog/viewWord.blade.php
@@ -9,7 +9,10 @@ $path = asset('storage/files/'.$name);
 
 <div class="d-flex" style="justify-content: center;flex-direction:column;align-items:center">
   <h2 style="text-align:center">Document file (*docx *xlsx *pptx)</h2>
-  <span>{{$name}}</span>
+  <div class="d-flex align-items-center">
+    <img src="{{url('/file-link.svg')}}" width="30px">
+    <span style="color: green;font-size:14px;font-weight:500">{{$name}}</span>
+  </div>
   <iframe style="width: 80%" height="1000vh" src="//docs.google.com/gview?url={{$path}}&embedded=true"></iframe>
 </div>
 </div>
